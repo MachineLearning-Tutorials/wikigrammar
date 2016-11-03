7f15fc92a7ba89ccdaf65710de27336f8ccb64ea
halfak <aaron.halfaker@gmail.com>
2016-11-03 09:05:39 -0500

    Init with working extract_sentences utility

---
+++ wikigrammar/functions.py
@@ -0,0 +1,22 @@
+import mwparserfromhell as mwparser
+from mwparserfromhell.nodes import Wikilink
+
+
+def clean_wikitext(sentence_text):
+    wikicode = mwparser.parse(sentence_text.strip())
+    stripped_text = strip_wikicode(wikicode)
+    return stripped_text.replace("\t", "\\t").replace("\n", "\\n")
+
+
+def strip_wikicode(wikicode):
+    return "".join(_strip_wikicode(wikicode))
+
+
+def _strip_wikicode(wikicode):
+
+    for node in wikicode.nodes:
+        stripped = node.__strip__(normalize=True, collapse=True)
+        if isinstance(node, Wikilink):
+            stripped = stripped.split("|")[-1]
+        if stripped is not None:
+            yield str(stripped)
---
+++ wikigrammar/utilities/extract_sentences.py
@@ -0,0 +1,88 @@
+"""
+Extracts sentences for a set of revisions provided.  Writes out a file where
+each line contains a complete sentence.  Tabs and linebreaks are escaped as
+`\t` and `\n` respectively.
+
+Usage:
+    extract_sentences --host=<url> [--input=<path>] [--output=<path>]
+                      [--workers=<num>]
+                      [--verbose] [--debug]
+
+Options:
+    -h --help        Print this documentation
+    --host=<url>     The host URL of a MediaWiki installation to extract
+                     text from
+    --input=<path>   Path to a file containing observations [default: <stdin>]
+    --output=<path>  Path to a file to write new observations to
+                     [default: <stdout>]
+    --workers=<num>  The number of parallel workers to spool up for sentence
+                     extraction [default: <cpu-count>]
+
+"""
+import logging
+import sys
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import cpu_count
+
+import docopt
+import mwapi
+from revscoring.extractors import api
+from revscoring.features import wikitext
+from revscoring.utilities.util import read_observations
+
+from ..functions import clean_wikitext
+
+
+def main(argv=None):
+    args = docopt.docopt(__doc__, argv=argv)
+
+    logging.basicConfig(
+        level=logging.WARNING if not args['--debug'] else logging.DEBUG,
+        format='%(asctime)s %(levelname)s:%(name)s -- %(message)s'
+    )
+
+    host = args['--host']
+
+    if args['--input'] == "<stdin>":
+        obs = read_observations(sys.stdin)
+    else:
+        obs = read_observations(open(args['--input']))
+
+    if args['--output'] == "<stdout>":
+        output = sys.stdout
+    else:
+        output = open(args['--output'], 'w')
+
+    if args['--workers'] == "<cpu-count>":
+        workers = cpu_count()
+    else:
+        workers = int(args['--workers'])
+
+    run(host, obs, output, workers)
+
+
+def run(host, obs, output, workers):
+    extractor = api.Extractor(mwapi.Session(
+        host, user_agent="Wikigrammar sentence extractor"))
+
+    sentence_extractor = SentenceExtractor(extractor)
+    with ProcessPoolExecutor(max_workers=workers) as executor:
+        for sentences in executor.map(sentence_extractor.extract, obs):
+            for sentence in sentences:
+                output.write(sentence)
+                output.write("\n")
+
+
+class SentenceExtractor:
+
+    def __init__(self, extractor):
+        self.extractor = extractor
+
+    def extract(self, ob):
+        # Extract sentences
+        sentences = self.extractor.extract(
+            ob['rev_id'], wikitext.revision.datasources.sentences)
+
+        # Clean them up
+        return [clean_wikitext("".join(str(t) for t in sentence))
+                for sentence in sentences]
